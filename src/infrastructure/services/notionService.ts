d243b3ec43b38997fa127bf74cb20449d087c323
Shukur Sapaev <shukursapaev@gmail.com>
2025-06-08 18:02:35 +0500

    Remove unused legacy layers

--- src/infrastructure/services/notionService.ts
+++ src/infrastructure/services/notionService.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '../../domain/entity/transaction';
+import { Transaction } from '../../modules/transaction/domain/transactionEntity';
 import { Client } from '@notionhq/client';
 import dotenv from 'dotenv';
 dotenv.config();
