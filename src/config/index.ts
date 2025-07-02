dedefb7c3171b3b7b16e7ed53fe17513d4991147
Shukur Sapaev <shukursapaev@gmail.com>
2025-07-02 17:52:09 +0500

    Update web app domain

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,5 +6,5 @@ export const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '';
 export const NOTION_API_KEY = process.env.NOTION_API_KEY || '';
 export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID || '';
 export const TG_BOT_API_KEY = process.env.TG_BOT_API_KEY || '';
-export const WEB_APP_URL = process.env.WEB_APP_URL || '';
+export const WEB_APP_URL = process.env.WEB_APP_URL || 'https://sapaev.uz';
 
